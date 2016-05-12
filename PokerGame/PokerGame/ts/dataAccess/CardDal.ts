17801ce4780505ccbd5b06351faec9d451c7cb44
sxzhou <sxzhou@advent.com>
2016-05-12 17:29:40 +0800

    get card png source

--- PokerGame/PokerGame/ts/dataAccess/CardDal.ts
+++ PokerGame/PokerGame/ts/dataAccess/CardDal.ts
@@ -1,7 +1,31 @@
-﻿import {Card} from '../model/Card'
+﻿import {Card} from '../model/Card';
+import {CardSuite} from '../model/CardSuite';
+import {CardFace} from '../model/CardFace';
 export class CardDal {
+    private static mapToCardImageName(card: Card): string {
+        let facePart: string;
+        if (card.face == CardFace.Ace) {
+            facePart = "ace";
+        } else if (card.face == CardFace.Jack) {
+            facePart = "jack";
+        } else if (card.face == CardFace.Queen) {
+            facePart = "queen";
+        } else if (card.face == CardFace.King) {
+            facePart = "king";
+        } else {
+            facePart = card.face.toString();
+        }
+
+        let suitePart: string = CardSuite[card.suite];
+
+        return facePart + "_of_" + suitePart;
+    }
+
     static getCardPNGImage(card: Card): string {
-        return "./resource/cards/png/10_of_clubs.png";
+        let pngBase = "./resource/cards/png/";
+        let suffix = ".png";
+        var name = CardDal.mapToCardImageName(card);
+        return pngBase + name + suffix;
     }
 
     static getCardSVGImage(card: Card): string {
